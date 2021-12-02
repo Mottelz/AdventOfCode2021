86bd0dd0e5e0760bf92d742184568731b1a9e9d2
Mottelz <shachna@gmail.com>
2021-12-02 09:21:05 -0500

    completed day 2

---
+++ Day2/main.py
@@ -0,0 +1,59 @@
+from utils import read_file_by_line
+from typing import List
+
+
+def process_instruction_for_part1(direction: str, magnitude: int, depth: int, horizontal: int) -> (int, int):
+    if direction == 'forward':
+        horizontal += magnitude
+    elif direction == 'backward':
+        horizontal -= magnitude
+    elif direction == 'down':
+        depth += magnitude
+    elif direction == 'up':
+        depth -= magnitude
+    return depth, horizontal
+
+
+def process_instruction_for_part2(direction: str, magnitude: int, depth: int, horizontal: int, aim: int) -> (int, int):
+    if direction == 'forward':
+        horizontal += magnitude
+        depth += magnitude*aim if aim != 0 else 0
+    elif direction == 'backward':
+        horizontal -= magnitude
+        depth -= magnitude * aim if aim != 0 else 0
+    elif direction == 'down':
+        aim += magnitude
+    elif direction == 'up':
+        aim -= magnitude
+    return depth, horizontal, aim
+
+
+def parse_instruction(raw: str) -> (str, int):
+    out = raw.split()
+    return out[0], int(out[1])
+
+
+def part1(data: List[str]) -> int:
+    depth = horizontal = 0
+    for instruction in data:
+        direction, magnitude = parse_instruction(instruction)
+        depth, horizontal = process_instruction_for_part1(direction, magnitude, depth, horizontal)
+    return depth*horizontal
+
+
+def part2(data: List[str]) -> int:
+    depth = horizontal = aim = 0
+    for instruction in data:
+        direction, magnitude = parse_instruction(instruction)
+        depth, horizontal, aim = process_instruction_for_part2(direction, magnitude, depth, horizontal, aim)
+    return depth*horizontal
+
+
+def main():
+    data = read_file_by_line("input.txt")
+    print(f"Part 1: {part1(data)}")
+    print(f"Part 2: {part2(data)}")
+
+
+if __name__ == '__main__':
+    main()
